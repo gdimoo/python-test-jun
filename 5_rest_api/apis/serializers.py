b664bae0a980c892f7cdc44af44523bcc6cbceb1
gdimoo <siripra999@gmail.com>
2024-06-10 23:44:43 +0700

    migrations

--- 5_rest_api/apis/serializers.py
+++ 5_rest_api/apis/serializers.py
@@ -1,17 +1,14 @@
 from rest_framework import serializers
 from .models import School, Classroom, Teacher, Student
 
-class SchoolSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = School
-        fields = '__all__'
-
 class ClassroomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Classroom
         fields = '__all__'
 
 class TeacherSerializer(serializers.ModelSerializer):
+    classrooms = ClassroomSerializer(many=True)
+
     class Meta:
         model = Teacher
         fields = '__all__'
@@ -20,3 +17,19 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
+
+class SchoolSerializer(serializers.ModelSerializer):
+    classrooms = ClassroomSerializer(many=True, read_only=True)
+    classroom_count = serializers.IntegerField(source='classrooms.count', read_only=True)
+    teacher_count = serializers.SerializerMethodField()
+    student_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = School
+        fields = '__all__'
+
+    def get_teacher_count(self, obj):
+        return Teacher.objects.filter(classrooms__school=obj).distinct().count()
+
+    def get_student_count(self, obj):
+        return Student.objects.filter(classroom__school=obj).count()
