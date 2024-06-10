ad6828d0529ffd82f677a7df47f375f15dcbd416
gdimoo <siripra999@gmail.com>
2024-06-11 00:48:42 +0700

    show models in admin page

--- 5_rest_api/apis/admin.py
+++ 5_rest_api/apis/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import School, Classroom, Teacher, Student
+
+admin.site.register(School)
+admin.site.register(Classroom)
+admin.site.register(Teacher)
+admin.site.register(Student)
